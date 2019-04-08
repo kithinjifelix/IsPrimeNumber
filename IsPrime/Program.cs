69c85c4ce7dc4279f591f5d79793919d4399d9f9
Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>
2019-04-08 18:50:58 +0300

    function will return if the number isprime or not

---
+++ IsPrime/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPrime(73));
+            Console.ReadKey();
+        }
+
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1) return false;
+            if (n == 2) return true;
+            if (n % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(n));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
